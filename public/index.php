3a6a9ba2bded612f4de74bc81a2232f0e701e840
sitrakathierry <sitrakathierryfr@gmail.com>
2023-02-17 05:29:03 +0300

    cross-origin

--- public/index.php
+++ public/index.php
@@ -1,8 +1,18 @@
 <?php
-
+if ($_SERVER['APP_DEBUG']) {
+    header('Access-Control-Allow-Origin:' . rtrim($_SERVER['HTTP_REFERER'], '/'));
+} else {
+    header('Access-Control-Allow-Origin:www.hikamsm.com');
+}
+header('Access-Control-Allow-Headers:*');
+header('Access-Control-Allow-Credentials:true');
+header('Access-Control-Allow-Headers:X-Requested-With, Content-Type, withCredentials');
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    die();
+}
 use App\Kernel;
-
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
