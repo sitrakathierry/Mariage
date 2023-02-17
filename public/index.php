05a65695a1d4695035e4ebd60a57ad557cb21c7d
sitrakathierry <sitrakathierryfr@gmail.com>
2023-02-17 05:30:37 +0300

    fqsd0

--- public/index.php
+++ public/index.php
@@ -1,19 +1,21 @@
 <?php
-if ($_SERVER['APP_DEBUG']) {
-    header('Access-Control-Allow-Origin:' . rtrim($_SERVER['HTTP_REFERER'], '/'));
-} else {
-    header('Access-Control-Allow-Origin:www.hikamsm.com');
-}
-header('Access-Control-Allow-Headers:*');
-header('Access-Control-Allow-Credentials:true');
-header('Access-Control-Allow-Headers:X-Requested-With, Content-Type, withCredentials');
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    die();
-}
+
 use App\Kernel;
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 
 return function (array $context) {
+    if ($context['APP_DEBUG']) {
+        header('Access-Control-Allow-Origin:' . rtrim($context['HTTP_REFERER'], '/'));
+    } else {
+        header('Access-Control-Allow-Origin:www.hikamsm.com');
+    }
+    header('Access-Control-Allow-Headers:*');
+    header('Access-Control-Allow-Credentials:true');
+    header('Access-Control-Allow-Headers:X-Requested-With, Content-Type, withCredentials');
+    if ($context['REQUEST_METHOD'] === 'OPTIONS') {
+        die();
+    }
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
