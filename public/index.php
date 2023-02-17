e6916fc96aa04345ab255bb32bd87e1dc18a57a5
sitrakathierry <sitrakathierryfr@gmail.com>
2023-02-17 05:36:28 +0300

    sd

--- public/index.php
+++ public/index.php
@@ -6,16 +6,5 @@ require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 
 return function (array $context) {
-    if ($context['APP_DEBUG']) {
-        header('Access-Control-Allow-Origin:' . rtrim($context['HTTP_REFERER'], '/'));
-    } else {
-        header('Access-Control-Allow-Origin:www.hikamsm.com/Mariage');
-    }
-    header('Access-Control-Allow-Headers:*');
-    header('Access-Control-Allow-Credentials:true');
-    header('Access-Control-Allow-Headers:X-Requested-With, Content-Type, withCredentials');
-    if ($context['REQUEST_METHOD'] === 'OPTIONS') {
-        die();
-    }
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
