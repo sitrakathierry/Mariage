e05014be2ec1475b1b407028823cb8209a48db2c
sitrakathierry <sitrakathierryfr@gmail.com>
2023-02-17 06:18:15 +0300

    config

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
